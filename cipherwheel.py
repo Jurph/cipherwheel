50989e597cde32816c9fd07bed660bf5fa14b9b5
Jurph <j.r.parsons@gmail.com>
2018-12-03 20:22:02 -0500

    Got the null rotor working.

--- cipherwheel.py
+++ cipherwheel.py
@@ -1,24 +1,40 @@
-import string
+from string import ascii_uppercase, digits
+from random import shuffle
 
 # Constants
-alphabet = list(map(chr,range(65, 91))) + list(map(chr, range(48, 58)))  # Defines a zero-indexed list [A-Z,0-9]
+alphabet = list(ascii_uppercase + digits)  # Defines a zero-indexed list [A-Z,0-9]
+
+def random_monoalpha_cipher(pool=None):
+    """Generates a Monoalphabetic Cipher as a dictionary"""
+    if pool is None:
+        pool = ascii_uppercase + digits
+    original_pool = list(pool)
+    shuffled_pool = list(pool)
+    shuffle(shuffled_pool)
+    return dict(zip(original_pool, shuffled_pool))
+
+def inverse_monoalpha_cipher(monoalpha_cipher):
+    """Given a Monoalphabetic Cipher (dictionary) return the inverse."""
+    inverse_monoalpha = {}
+    for key, value in monoalpha_cipher.iteritems():
+        inverse_monoalpha[value] = key
+    return inverse_monoalpha
+
+def encrypt_with_monoalpha(message, monoalpha_cipher):
+    encrypted_message = []
+    for letter in message:
+        encrypted_message.append(monoalpha_cipher.get(letter, letter))
+    return ''.join(encrypted_message)
+
+def decrypt_with_monoalpha(encrypted_message, monoalpha_cipher):
+    return encrypt_with_monoalpha(
+               encrypted_message,
+               inverse_monoalpha_cipher(monoalpha_cipher)
+           )
 
 
-# Functions
-def encrypt(plaintext, key):
-    newalphabet = makealphabet(key)
-    ciphertext = map(plaintext.upper(), newalphabet)
-    return ciphertext
-
-
-def decrypt(ciphertext, key):
-    plaintext = ciphertext.upper()
-    return plaintext
-
-
-def addoffset(plainchar, keyletter):
-    offset = (ord(str(plainchar)) + ord(str(keyletter)))%36
-    shiftedchar = alphabet[offset]
+def addoffset(plainchar, offset_int):
+    shiftedchar = alphabet[(alphabet.index(plainchar) + offset_int)%36]
     return shiftedchar
 
 
@@ -30,28 +46,35 @@ def makealphabet(key):
     # for each character we return a list of 36 offsets that can be applied to each character.
     keylength = int(len(key))
 
-    # TODO: put these in an array so I can condense the keying IF/ELSE below
-    rotor_1_offset = 24  # When [key1] is 24 (X) the odd slots are null.
-    rotor_2_offset = 34  # When [key1] - [key2] is -2 (or -3) the even slots are null.
-    rotor_3_offset = 14  # Not used yet
-    rotor_4_offset = 4   # Not used yet
-    newalphabet = list()
-    for letter in range(1, 36):
+    rotors = [13, 35, 17, 3]
+
+    rotor_settings = [0] * 36
+
+    # With two rotors, modulo is always 0 or 1
+    #
+    for letter in range(0, 36):
         if letter%keylength == 0:
-            newalphabet[letter] = addoffset(alphabet[letter], rotor_1_offset)
+            rotor_settings[letter] = addoffset(alphabet[letter], (rotors[0] + alphabet.index(key[0])))
         elif letter%keylength == 1:
-            newalphabet[letter] = addoffset(alphabet[letter], rotor_2_offset)
+            rotor_settings[letter] = addoffset(alphabet[letter], (rotors[1] + alphabet.index(key[1])))
         elif letter%keylength == 2:
-            newalphabet[letter] = addoffset(alphabet[letter], rotor_3_offset)
+            rotor_settings[letter] = addoffset(alphabet[letter], rotors[2])
+        elif letter%keylength == 3:
+            rotor_settings[letter] = addoffset(alphabet[letter], rotors[3])
         else:
             exit()
-    return newalphabet
+
+
+
+    return rotor_settings
 
 
 if __name__ == "__main__":
-    plaintext = "tHiS iS a TEst 123 456"
-    key = 'QZ'
-    ciphertext = encrypt(plaintext, key)
-    print("Ciphertext using key {} is:\n {}".format(key, ciphertext))
-    plaintext = decrypt(ciphertext, key)
-    print("Plaintext using key {} is:\n {}".format(key, ciphertext))
+    print("Unit tests for incrementing A, Z, 0, and 9 by 7.")
+    print(addoffset("A", 7))
+    print(addoffset("Z", 7))
+    print(addoffset("0", 7))
+    print(addoffset("9", 7))
+    print("This should look like an all-caps alphabet: \n")
+    newsetting = makealphabet("XB")
+    print(newsetting)
