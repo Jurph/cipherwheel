377cec645bd743b6e32e98c49319e6f02d671272
Jurph <j.r.parsons@gmail.com>
2018-12-03 21:55:26 -0500

    Got the null rotor working. Reformatted and credited Ballestrini's work.

--- cipherwheel.py
+++ cipherwheel.py
@@ -4,35 +4,6 @@ from random import shuffle
 # Constants
 alphabet = list(ascii_uppercase + digits)  # Defines a zero-indexed list [A-Z,0-9]
 
-def random_monoalpha_cipher(pool=None):
-    """Generates a Monoalphabetic Cipher as a dictionary"""
-    if pool is None:
-        pool = ascii_uppercase + digits
-    original_pool = list(pool)
-    shuffled_pool = list(pool)
-    shuffle(shuffled_pool)
-    return dict(zip(original_pool, shuffled_pool))
-
-def inverse_monoalpha_cipher(monoalpha_cipher):
-    """Given a Monoalphabetic Cipher (dictionary) return the inverse."""
-    inverse_monoalpha = {}
-    for key, value in monoalpha_cipher.iteritems():
-        inverse_monoalpha[value] = key
-    return inverse_monoalpha
-
-def encrypt_with_monoalpha(message, monoalpha_cipher):
-    encrypted_message = []
-    for letter in message:
-        encrypted_message.append(monoalpha_cipher.get(letter, letter))
-    return ''.join(encrypted_message)
-
-def decrypt_with_monoalpha(encrypted_message, monoalpha_cipher):
-    return encrypt_with_monoalpha(
-               encrypted_message,
-               inverse_monoalpha_cipher(monoalpha_cipher)
-           )
-
-
 def addoffset(plainchar, offset_int):
     shiftedchar = alphabet[(alphabet.index(plainchar) + offset_int)%36]
     return shiftedchar
@@ -45,31 +16,59 @@ def makealphabet(key):
     # message key to apply further offsets to the alphabet subsets. Once we have a final offset
     # for each character we return a list of 36 offsets that can be applied to each character.
     keylength = int(len(key))
-
     rotors = [13, 35, 17, 3]
-
     rotor_settings = [0] * 36
-
     # With two rotors, modulo is always 0 or 1
-    #
     for letter in range(0, 36):
         if letter%keylength == 0:
             rotor_settings[letter] = addoffset(alphabet[letter], (rotors[0] + alphabet.index(key[0])))
         elif letter%keylength == 1:
             rotor_settings[letter] = addoffset(alphabet[letter], (rotors[1] + alphabet.index(key[1])))
         elif letter%keylength == 2:
-            rotor_settings[letter] = addoffset(alphabet[letter], rotors[2])
+            rotor_settings[letter] = addoffset(alphabet[letter], rotors[2] + alphabet.index(key[2]))
         elif letter%keylength == 3:
-            rotor_settings[letter] = addoffset(alphabet[letter], rotors[3])
+            rotor_settings[letter] = addoffset(alphabet[letter], rotors[3] + alphabet.index(key[3]))
         else:
-            exit()
-
-
-
+            print("This system only supports 1-4 rotors")
+            exit(1)
     return rotor_settings
 
 
+#  (RB) These functions from R. Ballestrini
+def random_monoalpha_cipher(pool=None):
+    """Generates a Monoalphabetic Cipher as a dictionary"""
+    if pool is None:
+        pool = ascii_uppercase + digits
+    original_pool = list(pool)
+    shuffled_pool = list(pool)
+    shuffle(shuffled_pool)
+    return dict(zip(original_pool, shuffled_pool))
+
+
+def inverse_monoalpha_cipher(monoalpha_cipher):
+    """Given a Monoalphabetic Cipher (dictionary) return the inverse."""
+    inverse_monoalpha = {}
+    for key, value in monoalpha_cipher.iteritems():
+        inverse_monoalpha[value] = key
+    return inverse_monoalpha
+
+
+def encrypt_with_monoalpha(message, monoalpha_cipher):
+    encrypted_message = []
+    for letter in message:
+        encrypted_message.append(monoalpha_cipher.get(letter, letter))
+    return ''.join(encrypted_message)
+
+
+def decrypt_with_monoalpha(encrypted_message, monoalpha_cipher):
+    return encrypt_with_monoalpha(
+               encrypted_message,
+               inverse_monoalpha_cipher(monoalpha_cipher)
+           )
+
+
 if __name__ == "__main__":
+    # Just run unit tests if run standalone
     print("Unit tests for incrementing A, Z, 0, and 9 by 7.")
     print(addoffset("A", 7))
     print(addoffset("Z", 7))
