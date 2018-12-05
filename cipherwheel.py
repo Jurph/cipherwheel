e1ca6d7d69fb4bac4f23509d900ae8cb3bf17177
Jurph <j.r.parsons@gmail.com>
2018-12-05 11:50:49 -0500

    Looking at some existing methods like collections.deque.rotate and str.translate to simplify.

--- cipherwheel.py
+++ cipherwheel.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 from string import ascii_uppercase, digits
 from random import shuffle
+from collections import deque
 
 # Constants
 alphabet = list(ascii_uppercase + digits)  # Defines a zero-indexed list [A-Z,0-9]
@@ -11,27 +13,39 @@ def addoffset(plainchar, offset_int):
 
 def makealphabet(key):
     # We make an alphabet by splitting the alphabet into lists of characters, dividing the
-    # 36-character set into 1, 2, 3, or 4 rotors. Then we use the rotor_N_offset to shift
-    # those letters based on the rotor's built-in offsets (hard-coded here). Then we use the
-    # message key to apply further offsets to the alphabet subsets. Once we have a final offset
+    # 36-character set into 1, 2, 3, or 4 rotors. We use the array of rotor offsets to shift
+    # those letters to their "AA" configuration. This simulates the hardware configuration of
+    # our two-rotor cipher wheel at "0,0". (There are 36x18 possibilities and rotors[] chooses ours.)
+    # Once we have our hardware offsets, we simulate turning the rotors - that is, applying the
+    # keys - the same way our wheel does. The first letter of the key determines the offset of
+    # the plaintext letter "A" from the ciphertext letter "A": a classic Caesar Cipher offset (ROT36)
+    # where knowing the shift value gives you all other letters. The second letter applies a relative
+    # shift to the "demi-alphabet", moving "B" in the plaintext relative to "A" in the plaintext
+    # so instead of +1 it will be at +3, +5, and so on up to +35 (or -1).
     # for each character we return a list of 36 offsets that can be applied to each character.
+
+    output_alphabet = [0] * 36  # Initialize output alphabet to contain no letters
     keylength = int(len(key))
-    rotors = [13, 35, 17, 3]
-    rotor_settings = [0] * 36
-    # With two rotors, modulo is always 0 or 1
+    rotors = [-14, 24, 0, 0]   # Configure the hardware rotors (choose your own keys w/ you and your friends)
+
+    str_alphabet = ascii_uppercase + digits
+    first_shift = str_alphabet.deque.rotate(rotors[0])
+
+
     for letter in range(0, 36):
+        output_alphabet[letter] = addoffset(alphabet[letter], (rotors[0] + alphabet.index(key[0])))  # Always apply R1 offset
         if letter%keylength == 0:
-            rotor_settings[letter] = addoffset(alphabet[letter], (rotors[0] + alphabet.index(key[0])))
+            output_alphabet[letter] = addoffset(alphabet[letter], int((rotors[1] + alphabet.index(key[1])+1)/2))  # R2 offset
         elif letter%keylength == 1:
-            rotor_settings[letter] = addoffset(alphabet[letter], (rotors[1] + alphabet.index(key[1])))
+            output_alphabet[letter] = addoffset(alphabet[letter], 0)  # No shift for odd letters
         elif letter%keylength == 2:
-            rotor_settings[letter] = addoffset(alphabet[letter], rotors[2] + alphabet.index(key[2]))
+            print()  # TODO
         elif letter%keylength == 3:
-            rotor_settings[letter] = addoffset(alphabet[letter], rotors[3] + alphabet.index(key[3]))
+            print()  # TODO
         else:
             print("This system only supports 1-4 rotors")
             exit(1)
-    return rotor_settings
+    return output_alphabet
 
 
 #  (RB) These functions from R. Ballestrini
@@ -74,6 +88,12 @@ if __name__ == "__main__":
     print(addoffset("Z", 7))
     print(addoffset("0", 7))
     print(addoffset("9", 7))
-    print("This should look like an all-caps alphabet: \n")
-    newsetting = makealphabet("XB")
+    print("Using key 'XW' this should look like an all-caps alphabet:")
+    newsetting = makealphabet("XW")
+    print(newsetting)
+    print("Using key 'XV' this should look like an all-caps alphabet:")
+    newsetting = makealphabet("XV")
+    print(newsetting)
+    print("Using key 'AA' this should look like an all-caps mixed alphabet starting '1 O 3 Q 5 S 7...':")
+    newsetting = makealphabet("AA")
     print(newsetting)
