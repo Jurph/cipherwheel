dd8f85bfed9ee1562746b4a448864a7439ca1ccb
Jurph <j.r.parsons@gmail.com>
2018-12-05 14:20:26 -0500

    Trying a rotate(string,int) approach.

--- cipherwheel.py
+++ cipherwheel.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from string import ascii_uppercase, digits
 from random import shuffle
-from collections import deque
 
 # Constants
 alphabet = list(ascii_uppercase + digits)  # Defines a zero-indexed list [A-Z,0-9]
@@ -11,7 +10,13 @@ def addoffset(plainchar, offset_int):
     return shiftedchar
 
 
+def rotate(string, int):
+    newstring = string[int:] + string[:int]
+    return newstring
+
+
 def makealphabet(key):
+
     # We make an alphabet by splitting the alphabet into lists of characters, dividing the
     # 36-character set into 1, 2, 3, or 4 rotors. We use the array of rotor offsets to shift
     # those letters to their "AA" configuration. This simulates the hardware configuration of
@@ -22,30 +27,39 @@ def makealphabet(key):
     # where knowing the shift value gives you all other letters. The second letter applies a relative
     # shift to the "demi-alphabet", moving "B" in the plaintext relative to "A" in the plaintext
     # so instead of +1 it will be at +3, +5, and so on up to +35 (or -1).
-    # for each character we return a list of 36 offsets that can be applied to each character.
 
-    output_alphabet = [0] * 36  # Initialize output alphabet to contain no letters
-    keylength = int(len(key))
-    rotors = [-14, 24, 0, 0]   # Configure the hardware rotors (choose your own keys w/ you and your friends)
+    rotors = [9, 13, 1, 2]   # Configure the hardware rotors (choose your own keys w/ you and your friends)
+
+    keylength = int(len(key))  # Configure the "software" rotors - that is, the per-message key you're using
+
+    key_turns = []
+    for letter in key:
+        key_turns.append(alphabet.index(letter))
+    print("For key {} the key turns are:\n{}".format(key, key_turns))
 
     str_alphabet = ascii_uppercase + digits
-    first_shift = str_alphabet.deque.rotate(rotors[0])
 
-
-    for letter in range(0, 36):
-        output_alphabet[letter] = addoffset(alphabet[letter], (rotors[0] + alphabet.index(key[0])))  # Always apply R1 offset
-        if letter%keylength == 0:
-            output_alphabet[letter] = addoffset(alphabet[letter], int((rotors[1] + alphabet.index(key[1])+1)/2))  # R2 offset
-        elif letter%keylength == 1:
-            output_alphabet[letter] = addoffset(alphabet[letter], 0)  # No shift for odd letters
-        elif letter%keylength == 2:
-            print()  # TODO
-        elif letter%keylength == 3:
-            print()  # TODO
+    # Shift the demi-alphabets relative to one another
+    evens = ""
+    odds = ""
+    for letter in str_alphabet:
+        if str_alphabet.index(letter)%keylength == 0:
+            evens += letter
         else:
-            print("This system only supports 1-4 rotors")
-            exit(1)
-    return output_alphabet
+            odds += letter
+
+    # Apply secondary rotor and secondary key digit
+    odds = rotate(odds, rotors[1] + key_turns[1])
+    # Shuffle the evens and odds back into an alphabet
+    demi_shift = ""
+    for letter in odds:
+        demi_shift += evens[odds.index(letter)]
+        demi_shift += odds[odds.index(letter)]
+
+    # Rotate the whole alphabet using the primary rotor and first key digit
+    whole_shift = list(rotate(demi_shift, (rotors[0] + key_turns[0])))
+    print(whole_shift)
+    return whole_shift
 
 
 #  (RB) These functions from R. Ballestrini
@@ -88,12 +102,14 @@ if __name__ == "__main__":
     print(addoffset("Z", 7))
     print(addoffset("0", 7))
     print(addoffset("9", 7))
+    print("This should look like the alphabet shifted 12 slots:")
+    print(rotate(alphabet, 12))
     print("Using key 'XW' this should look like an all-caps alphabet:")
     newsetting = makealphabet("XW")
-    print(newsetting)
+    # print(newsetting)
     print("Using key 'XV' this should look like an all-caps alphabet:")
     newsetting = makealphabet("XV")
-    print(newsetting)
+    # print(newsetting)
     print("Using key 'AA' this should look like an all-caps mixed alphabet starting '1 O 3 Q 5 S 7...':")
     newsetting = makealphabet("AA")
-    print(newsetting)
+    # print(newsetting)
